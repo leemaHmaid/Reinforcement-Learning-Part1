f0f5cc854b359c74baf154cb435abb3fe484aa25
leemaHamid <leemahamid99@gmail.com>
2025-05-09 22:15:08 +0300

    Created td prediciton funtion

--- src/model_Free/td.py
+++ src/model_Free/td.py
@@ -1,1 +1,27 @@
 # this is to implement the TD learning algorithm
+#TD(0)
+
+
+def td_predicition(env, policy, episodes, alpha=0.1, gamma=0.9):
+
+    V = {state: 0.0 for state in env.states}
+
+    for ep in range(episodes):
+        state = env.states[0]
+        done = env.is_terminal(state)
+        while not done:
+
+            action =  policy[state]
+            transitions = env.get_transition_probabilities(state, action)
+            _, next_state, reward = transitions[0]
+
+            td_target = reward + gamma * V[next_state]
+            td_error = td_target - V[state]
+            V[state] += alpha * td_error
+            state = next_state
+            done = env.is_terminal(state)
+
+    return V
+
+
+
