2d0f9b955da2295237c4550ef4f1d9aef7491d07
leemaHamid <leemahamid99@gmail.com>
2025-05-09 23:10:28 +0300

    fixed import using utils

--- tests/test_td_prediction.py
+++ tests/test_td_prediction.py
@@ -1,28 +1,11 @@
 import random
 from src.gridworld import GridWorld
-from src.model_Free.td import td_predicition
+from src.model_Free.td0 import td_predicition
 import matplotlib.pyplot as plt
 import numpy as np
 
-def print_value_function(V, grid_size):
-  for i in range(grid_size):
-      row = ""
-      for j in range(grid_size):
-          val = V.get((i, j), 0.0)
-          row += f"{val:6.2f} "
-      print(row)
-  print()
+from src.utils import plot_value_heatmap, print_value_function
  
-
-def plot_value_heatmap(V, grid_size):
-    grid = np.zeros((grid_size, grid_size))
-    for (i, j), v in V.items():
-        grid[i][j] = v
-
-    plt.imshow(grid, cmap='viridis', interpolation='nearest')
-    plt.colorbar(label='Value')
-    plt.title("Value Function Heatmap")
-    plt.show()
 
 def test_td():
     def random_policy(state):
