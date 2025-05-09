890ff973b1942ce66bce33324738d467bcc6e877
leemaHamid <leemahamid99@gmail.com>
2025-05-09 12:00:10 +0300

    Refactored main file to run tests from tests folder

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
- 
-from src.gridworld import GridWorld
-from src.dynamic_programming.policy_evaluation import policy_evaluation
-from src.dynamic_programming.policy_improvement import policy_improvement
-from src.dynamic_programming.policy_iteration import policy_iteration
-from src.dynamic_programming.value_iteration import value_iteration, extract_policy_from_value
- 
-from src.model_Free.monte_carlo import monte_carlo_prediction
-
- 
-
- 
 import random
 
 def print_value_table(value_table, env):
@@ -35,90 +23,44 @@ def print_policy(policy, env):
         print(" ".join(row))
 
 
-def test_gridworld():
-    env = GridWorld()
-    print("\n States in GridWorld:")
-    print(env.states)
-    print("\n Possible actions from (1, 1):")
-    print(env.get_possible_actions((1, 1)))
 
+import argparse
+import sys
 
-def test_policy_evaluation():
-    env = GridWorld()
+# Add test and source folders to import path
+sys.path.append("src")
+sys.path.append("tests")
 
-    #  Build a random policy for all non-terminal states
-    policy = {}
-    for state in env.states:
-        if env.is_terminal(state):
-            continue
-        actions = env.get_possible_actions(state)
-        if actions:
-            policy[state] = random.choice(actions)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", type=str, help="Which test to run (mc, pi, vi, pe, pim)")
+    args = parser.parse_args()
 
-    # 🔍 Evaluate the policy
-    value_table = policy_evaluation(env, policy, gamma=0.9, theta=1e-4)
+    if args.test == "mc":
+        from test_monte_carlo import test_monte_carlo
+        test_monte_carlo()
 
-    # Display result
-    print_value_table(value_table, env)
-    print_policy(policy, env)
+    elif args.test == "pi":
+        from test_policy_iteration import test_policy_iteration
+        test_policy_iteration()
 
+    elif args.test == "vi":
+        from test_value_iteration import test_value_iteration
+        test_value_iteration()
 
-def test_policy_improvement():
-    env = GridWorld()
+    elif args.test == "pe":
+        from test_policy_evaluation import test_policy_evaluation
+        test_policy_evaluation()
 
-    # Start from a random policy
-    policy = {}
-    for state in env.states:
-        if env.is_terminal(state):
-            continue
-        actions = env.get_possible_actions(state)
-        if actions:
-            policy[state] = random.choice(actions)
+    elif args.test == "pim":
+        from test_policy_improvement import test_policy_improvement
+        test_policy_improvement()
 
-    # Evaluate and improve
-    value_table = policy_evaluation(env, policy, gamma=0.9, theta=1e-4)
-    improved_policy = policy_improvement(env, value_table, gamma=0.9)
-
-    # Show results
-    print_value_table(value_table, env)
-    print_policy(improved_policy, env)
-
-def test_policy_iteration():
-    env = GridWorld()
-    policy, value_table = policy_iteration(env, gamma=0.9, theta=1e-4)
-
-    print_value_table(value_table, env)
-    print_policy(policy, env)
-
-def test_value_iteration():
-    env = GridWorld()
-    value_table = value_iteration(env, gamma=0.9, theta=1e-4)
-
-    print_value_table(value_table, env)
-    policy = extract_policy_from_value(env, value_table, gamma=0.9)
-    print_policy(policy, env)
-
-import random
-
-def random_policy(state):
-    env = GridWorld()
-    actions = env.get_possible_actions(state)
-    return random.choice(actions)
-
-
-def test_monte_carlo():
-    env = GridWorld()
-
-    V = monte_carlo_prediction(env, random_policy, episodes=500, gamma=0.9, first_visit=False)
-    for i in range(env.grid_size):
-        row = [round(V[(i, j)], 2) for j in range(env.grid_size)]
-        print(row)
+    else:
+        print("Unknown test. Use one of: mc, pi, vi, pe, pim")
 
 if __name__ == "__main__":
-    # Uncomment what you want to test
-    test_gridworld()
-    test_policy_evaluation()
-    test_policy_improvement()
-    test_policy_iteration()
-    test_value_iteration()
-    test_monte_carlo()
+    main()
+
+
+ 
